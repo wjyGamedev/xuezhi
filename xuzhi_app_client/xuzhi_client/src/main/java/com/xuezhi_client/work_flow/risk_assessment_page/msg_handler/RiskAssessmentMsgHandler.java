1725ea9de588e3179bddce7b63626400b555a4ed
XieZheng <xzworkone@126.com>
2015-09-23 02:23:52 +0800

    [modify]添加风险评估页面

---
+++ xuzhi_app_client/xuzhi_client/src/main/java/com/xuezhi_client/work_flow/risk_assessment_page/msg_handler/RiskAssessmentMsgHandler.java
@@ -0,0 +1,21 @@
+package com.xuezhi_client.work_flow.risk_assessment_page.msg_handler;
+
+import com.module.frame.BaseUIMsgHandler;
+import com.xuezhi_client.work_flow.risk_assessment_page.ui.RiskAssessmentActivity;
+
+/**
+ * Created by Administrator on 2015/9/23.
+ */
+public class RiskAssessmentMsgHandler extends BaseUIMsgHandler
+{
+	public RiskAssessmentMsgHandler(RiskAssessmentActivity activity)
+	{
+		super(activity);
+	}
+
+	public void saveRiskAssessmentInfo()
+	{
+
+		return;
+	}
+}
