11e1af99b1fbb404db5cd46a5fcb7942fd1117a8
XieZheng <xzworkone@126.com>
2015-09-23 01:34:40 +0800

    [modify]添加用户协议页面

--- xuzhi_app_client/xuzhi_client/src/main/java/com/xuezhi_client/work_flow/user_information_page/ui/UserInformationAcitivity.java
+++ xuzhi_app_client/xuzhi_client/src/main/java/com/xuezhi_client/work_flow/user_information_page/ui/UserInformationAcitivity.java
@@ -2,9 +2,15 @@ package com.xuezhi_client.work_flow.user_information_page.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.module.widget.bottom.BottomCommon;
+import com.module.widget.header.HeaderCommon;
+import com.xuezhi_client.work_flow.user_information_page.msg_handler.UserInformationMsgHandler;
 import com.xuzhi_client.xuzhi_app_client.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -12,6 +18,14 @@ import butterknife.ButterKnife;
  */
 public class UserInformationAcitivity extends Activity
 {
+	//widget
+	private                                   HeaderCommon m_headerCommon            = null;
+	private                                   BottomCommon m_bottomCommon            = null;
+	@Bind (R.id.user_information_text_legion) TextView     m_userInformationLegionTV = null;
+
+	//logical
+	private UserInformationMsgHandler m_userInformationMsgHandler = new UserInformationMsgHandler(this);
+	private ClickBottomBtn            m_clickBottomBtn            = new ClickBottomBtn();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -20,7 +34,34 @@ public class UserInformationAcitivity extends Activity
 		setContentView(R.layout.activity_user_information);
 		ButterKnife.bind(this);
 
-		//		init();
+		init();
+	}
+
+	/**
+	 * inner func
+	 */
+	private void init()
+	{
+		m_headerCommon = (HeaderCommon)getFragmentManager().findFragmentById(R.id.common_header_fragment);
+		m_headerCommon.setTitle(R.string.user_information_page_title_text);
+
+		m_bottomCommon = (BottomCommon)getFragmentManager().findFragmentById(R.id.common_bottom_fragment);
+		m_bottomCommon.getCommonBottomBtn().setText(R.string.go_back_home_btn_text);
+		m_bottomCommon.getCommonBottomBtn().setOnClickListener(m_clickBottomBtn);
+
+		m_userInformationLegionTV.setText(R.string.user_information_content_text);
+	}
+
+	/**
+	 * overrider func
+	 */
+	class ClickBottomBtn implements View.OnClickListener
+	{
+		@Override
+		public void onClick(View v)
+		{
+			m_userInformationMsgHandler.go2MainPage();
+		}
 	}
 
 }
