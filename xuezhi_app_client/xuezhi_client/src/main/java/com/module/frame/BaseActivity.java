06eef2469c92d9da84b7c69ba3b175e3fdefa9b4
wjygamedev <wjygamedev@126.com>
2015-11-12 21:12:47 +0800

    [modify]在baseactivity中，添加关闭小键盘。

--- xuezhi_app_client/xuezhi_client/src/main/java/com/module/frame/BaseActivity.java
+++ xuezhi_app_client/xuezhi_client/src/main/java/com/module/frame/BaseActivity.java
@@ -16,6 +16,10 @@ package com.module.frame;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.module.data.DGlobal;
 import com.umeng.analytics.MobclickAgent;
@@ -78,6 +82,32 @@ abstract public class BaseActivity extends FragmentActivity
 		super.onStop();
 	}
 
+	//作用：点击EditText文本框之外任何地方隐藏键盘
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev)
+	{
+		if (ev.getAction() == MotionEvent.ACTION_DOWN)
+		{
+			View v = getCurrentFocus();
+			if (isShouldHideInput(v, ev))
+			{
+
+				InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+				if (imm != null)
+				{
+					imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+				}
+			}
+			return super.dispatchTouchEvent(ev);
+		}
+		// 必不可少，否则所有的组件都不会有TouchEvent了
+		if (getWindow().superDispatchTouchEvent(ev))
+		{
+			return true;
+		}
+		return onTouchEvent(ev);
+	}
+
 	/**
 	 * 将当前content，放入全局管理器
 	 */
@@ -91,6 +121,29 @@ abstract public class BaseActivity extends FragmentActivity
 		DGlobal.GetInstance().clearupContext(this);
 	}
 
+	private boolean isShouldHideInput(View v, MotionEvent event)
+	{
+		if (v != null && (v instanceof EditText))
+		{
+			int[] leftTop = {0, 0};
+			//获取输入框当前的location位置
+			v.getLocationInWindow(leftTop);
+			int left = leftTop[0];
+			int top = leftTop[1];
+			int bottom = top + v.getHeight();
+			int right = left + v.getWidth();
+			if (event.getX() > left && event.getX() < right && event.getY() > top && event.getY() < bottom)
+			{
+				// 点击的是输入框区域，保留点击EditText的事件
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 	//	/**
 	//	 * eventbus handler
 	//	 */
