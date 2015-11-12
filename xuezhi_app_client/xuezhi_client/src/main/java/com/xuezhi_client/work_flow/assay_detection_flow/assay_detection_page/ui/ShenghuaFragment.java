ec7055e46269d12f1009f68cf87fb120ef85b0e8
wjygamedev <wjygamedev@126.com>
2015-11-12 14:02:23 +0800

    [add/modify]添加了frame中的BaseFragment。             修改化验检查页面

---
+++ xuezhi_app_client/xuezhi_client/src/main/java/com/xuezhi_client/work_flow/assay_detection_flow/assay_detection_page/ui/ShenghuaFragment.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.xuezhi_client.work_flow.assay_detection_flow.assay_detection_page.ui.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/11/12		WangJY		1.0.0		create
+ */
+
+package com.xuezhi_client.work_flow.assay_detection_flow.assay_detection_page.ui;
+
+public class ShenghuaFragment
+{
+}
