c913485fcee2aa10a13f60479c6189845b18df75
XieZheng <xzworkone@126.com>
2015-11-25 21:54:48 +0800

    [modify]显示信息应为11页

--- xuezhi_app_client/xuezhi_client/src/main/java/com/xuezhi_client/work_flow/assay_detection_flow/config/AssayDetectionConfig.java
+++ xuezhi_app_client/xuezhi_client/src/main/java/com/xuezhi_client/work_flow/assay_detection_flow/config/AssayDetectionConfig.java
@@ -41,7 +41,7 @@ public class AssayDetectionConfig
 	public final static String ASSAY_DETECTION_TYPE        = "assay_detection_type";
 	public final static int    CHART_X_AXIS_DEFAULT_LENGTH = 5;
 	public final static String CHART_X_AXIS_DEFAULT_NAME   = "DataSet";
-	public final static int    CHART_PAGE_NUM              = 10;
+	public final static int    CHART_PAGE_NUM              = 11;
 
 	//default
 	public final static double CHART_MIN_DEFAULT   = 0;
